ef4b44835a15345b142482b0bec4eb23153353e3
ruoxi pei <ruoxipei@MacBook-Air.local>
2026-07-22 03:34:08 +0800

    Fix card-draft save resume startup

--- index.21f8786a.js
+++ index.21f8786a.js
@@ -1,4 +1,4 @@
-System.register(["./application.47d3f12e.js"], function (_export, _context) {
+System.register(["./application.781f04a0.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
