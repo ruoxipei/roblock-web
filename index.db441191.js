ccafbc782b724d275efa2cfa9c30901eaa9c2913
ruoxi pei <ruoxipei@MacBook-Air.local>
2026-07-22 01:35:06 +0800

    Defer unused prefab downloads

--- index.db441191.js
+++ index.db441191.js
@@ -1,4 +1,4 @@
-System.register(["./application.f620f46a.js"], function (_export, _context) {
+System.register(["./application.6e64f446.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
