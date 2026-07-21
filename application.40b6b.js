2391f74a8e5c849b9974f0c7ceaa288b18915787
ruoxi pei <ruoxipei@MacBook-Air.local>
2026-07-21 23:09:45 +0800

    Enlarge the mobile assembly board

--- application.40b6b.js
+++ application.40b6b.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.b7195.json';
+          this.settingsPath = 'src/settings.1b74a.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
