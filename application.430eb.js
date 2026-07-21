57d935089fa9ecc6bab09834f34711d2e7d0e6a7
ruoxi pei <ruoxipei@MacBook-Air.local>
2026-07-22 00:12:38 +0800

    Change assembly board to 8 by 5

--- application.430eb.js
+++ application.430eb.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.f72da.json';
+          this.settingsPath = 'src/settings.06ca6.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.f592f.js
+++ index.f592f.js
@@ -1,4 +1,4 @@
-System.register(["./application.00924.js"], function (_export, _context) {
+System.register(["./application.430eb.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
