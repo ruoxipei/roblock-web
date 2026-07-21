36c2622dcb270d901c374e908156591134859b4a
ruoxi pei <ruoxipei@MacBook-Air.local>
2026-07-22 02:47:42 +0800

    Stabilize expedition restart and mobile loading

--- application.47d3f12e.js
+++ application.47d3f12e.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.116c625d.json';
+          this.settingsPath = 'src/settings.dd502879.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.71d5e168.js
+++ index.71d5e168.js
@@ -1,4 +1,4 @@
-System.register(["./application.6e64f446.js"], function (_export, _context) {
+System.register(["./application.47d3f12e.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
